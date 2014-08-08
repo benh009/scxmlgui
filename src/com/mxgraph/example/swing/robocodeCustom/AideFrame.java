1d89240dc55947545fb339717d34d8f14a84371c
hofbauer <hofbauer92@gmail.com>
2014-08-08 20:14:34 +0200

    initialisation version 1 + custom action

---
+++ src/com/mxgraph/example/swing/robocodeCustom/AideFrame.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mxgraph.example.swing.robocodeCustom;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author hofbauer
+ */
+public class AideFrame extends JFrame {
+    public AideFrame()
+    {
+        super();
+    }
+    
+}
---
+++ src/com/mxgraph/example/swing/robocodeCustom/TestFrame.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mxgraph.example.swing.robocodeCustom;
+
+/**
+ *
+ * @author hofbauer
+ */
+public class TestFrame {
+    
+    
+    public static void main(String[] args)
+    {
+        AideFrame aide = new AideFrame();
+        
+    }
+}
